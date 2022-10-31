d0bec809d084b6502064606db46068a3602ac874
juanCsacasa <110007393+juanCsacasa@users.noreply.github.com>
2022-10-31 17:22:26 -0600

    practica dos

---
+++ nom.js
@@ -0,0 +1,37 @@
+let numero = 30; 
+let numero2 = 10;
+let alrevez = "juan"
+let vacio = ''
+
+/*function par_impar(x) {
+    if (x % 2 == 0) {
+        console.log("es par");
+    }else{
+        console.log("es impar");
+    }
+    if (x % 3 == 0) {
+        console.log("es divisible entre 3");
+    }else{
+        console.log("no es divisible entre 3");
+    }
+    if (x % 5 == 0) {
+        console.log("es divisible entre 5");
+    }else{
+        console.log("no es divisible entre 5");
+    }
+}
+par_impar(numero)*/
+
+/*function suma(y) {
+    for (let i = 1; i <= y; i++) {
+        console.log(i)
+    }
+}
+suma(numero2);*/
+
+function palabra(p) {
+    let i = p.length - 1;
+    while (i >= 0) {
+        
+  }
+}
