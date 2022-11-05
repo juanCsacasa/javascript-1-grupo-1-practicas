474ea6f4b31cbfb5e67cbd8ca0442e1de5ed6d18
juan sacasa <juanksacasa@gmail.com>
2022-11-05 00:10:11 -0600

    ejercicioo3

--- nom.js
+++ nom.js
@@ -1,37 +1,50 @@
-let numero = 30; 
+let numero = 30;
 let numero2 = 10;
-let alrevez = "juan"
-let vacio = ''
+let alrevez = "juan";
+let vacio = '';
+//let vocales = (a, e, i, o, u);
 
-/*function par_impar(x) {
-    if (x % 2 == 0) {
-        console.log("es par");
-    }else{
-        console.log("es impar");
-    }
-    if (x % 3 == 0) {
-        console.log("es divisible entre 3");
-    }else{
-        console.log("no es divisible entre 3");
-    }
-    if (x % 5 == 0) {
-        console.log("es divisible entre 5");
-    }else{
-        console.log("no es divisible entre 5");
-    }
-}
-par_impar(numero)*/
-
-/*function suma(y) {
-    for (let i = 1; i <= y; i++) {
-        console.log(i)
-    }
-}
-suma(numero2);*/
-
-function palabra(p) {
-    let i = p.length - 1;
-    while (i >= 0) {
-        
+function par_impar(x) {
+  if (x % 2 == 0) {
+    console.log("es par");
+  } else {
+    console.log("es impar");
+  }
+  if (x % 3 == 0) {
+    console.log("es divisible entre 3");
+  } else {
+    console.log("no es divisible entre 3");
+  }
+  if (x % 5 == 0) {
+    console.log("es divisible entre 5");
+  } else {
+    console.log("no es divisible entre 5");
   }
 }
+
+function suma(y) {
+  for (let i = 1; i <= y; i++) {
+    console.log(i)
+  }
+}
+
+function palabra(p) {
+  let i = p.length - 1;
+  while (i >= 0) {
+    vacio += p[i];
+    --i
+  }
+  return vacio;
+}
+
+
+/*function changed_i(params) {
+
+}*/
+
+function main2() {
+  console.log(par_impar(numero));
+  console.log(suma(numero2));
+  console.log(palabra(alrevez));
+}
+main2()
