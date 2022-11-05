8a75d828c76b637268adb4754bb38e0ae1b58912
juanCsacasa <110007393+juanCsacasa@users.noreply.github.com>
2022-11-05 10:13:52 -0600

    solucion de los problemas  y correccion

--- nom.js
+++ nom.js
@@ -2,8 +2,9 @@ let numero = 30;
 let numero2 = 10;
 let alrevez = "juan";
 let vacio = '';
-//let vocales = (a, e, i, o, u);
+let params = 'los ajas'
 
+//ejercicio1
 function par_impar(x) {
   if (x % 2 == 0) {
     console.log("es par");
@@ -21,13 +22,16 @@ function par_impar(x) {
     console.log("no es divisible entre 5");
   }
 }
-
+//ejercicio2
 function suma(y) {
+  let suma = 0;
   for (let i = 1; i <= y; i++) {
-    console.log(i)
+    suma += i
+    //console.log(i)
   }
+  return suma;
 }
-
+//ejercicio3
 function palabra(p) {
   let i = p.length - 1;
   while (i >= 0) {
@@ -36,15 +40,26 @@ function palabra(p) {
   }
   return vacio;
 }
+//ejercicio4_solucion profe julio
+function changed_i(params) {
+let resultado = '';
 
+  for (let i = 0;  i < params.length; i++) {
+    let letra = params[i];
 
-/*function changed_i(params) {
-
-}*/
+    if (letra == 'a' || letra == 'e'  || letra == 'i' ||letra == 'o'  || letra == 'u' ) {
+      resultado += 'i';
+    }else{
+      resultado += letra;
+    }
+  }
+  return resultado
+}
 
 function main2() {
-  console.log(par_impar(numero));
+  par_impar(numero);
   console.log(suma(numero2));
   console.log(palabra(alrevez));
+  console.log(changed_i(params))
 }
 main2()
